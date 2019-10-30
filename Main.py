1f2537150d228593edac591b8d5110fb5ad7879b
Preyas-Saxena <preyas.saxena@gmail.com>
2019-10-30 13:22:36 +0100

    Add files via upload

--- Main.py
+++ Main.py
@@ -9,18 +9,22 @@ import Data_processing
 
 device=torch.device('cuda' if torch.cuda.is_available else 'cpu')
 rootdir='/data/leuven/329/vsc32927/polyvore/'
+train_file='valid_no_dup_trim.json'
+valid_file='valid_no_dup_trim.json'
 
-normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-transform_list=transforms.Compose([transforms.Scale(224), transforms.CenterCrop(224), transforms.RandomHorizontalFlip(), transforms.ToTensor(), normalize,])
+#Actual Values from the training dataset:
+mean_data=[0.7170, 0.6794, 0.6613]
+std_data=[0.2358, 0.2511, 0.2574]
 
-num_epochs=1
-learning_rate=1e-3
+normalize = transforms.Normalize(mean=mean_data, std=std_data)
+transform_list=transforms.Compose([transforms.Scale(224), transforms.CenterCrop(224), transforms.ToTensor(), normalize,])
+
+num_epochs=10
+learning_rate=1e-4
 
 #Output is a variable containing tuples of (image,pos_image) pairs
-data=Data_processing.datapairs(datadir=rootdir,filename='train_no_dup.json',transforms=transform_list)
-
-#Getting the positive image pairs in batches
-train_loader=DataLoader(data,batch_size=64,shuffle=True)
+data=Data_processing.datapairs_updated(datadir=rootdir,filename=train_file,transforms=transform_list)
+train_loader=DataLoader(data,batch_size=10,shuffle=False)
 
 #Initializing a vgg net model with an embedding of 20 size, using a pre-trained model but will update all the weights
 vgg = Model.initialize_model("vgg", 128, feature_extract=False, use_pretrained=True)
@@ -30,14 +34,6 @@ vgg.to(device)
 optimizer=optim.Adam(vgg.parameters(),lr=learning_rate)
 
 #Training:
-Model.train_model(vgg, train_loader, optimizer=optimizer, epochs=num_epochs, device=device)
-
-#Generating sample embedding for an image from training data:
-sample=data.__getitem__(0)[0]
-sample=sample.unsqueeze(0)
-sample=sample.to(device=device)
-
-print(vgg(sample))
-print("Done and dusted")
-
+Model.train_model_oneneg(rootdir=rootdir, model=vgg ,dataloader=train_loader, optimizer=optimizer,epochs=num_epochs, device=device, validation_file=valid_file)
+print("Done")
 
