eb766d56d16f4f9954640a3b2b172d95a70c20f3
Preyas-Saxena <preyas.saxena@gmail.com>
2020-05-29 14:18:26 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,16 +1,19 @@
-
-# Do the import
 import torch
-import Model
-import torch.optim as optim
+import Data_processing
 from torch.utils.data import DataLoader
 from torchvision import transforms
-import Data_processing
+import Model
+import torch.optim as optim
+from torch.optim.lr_scheduler import StepLR
 
 device=torch.device('cuda' if torch.cuda.is_available else 'cpu')
+# device='cpu'
+
+# rootdir='/home/preyas/Desktop/Thesis/polyvore/'
 rootdir='/data/leuven/329/vsc32927/polyvore/'
-train_file='valid_no_dup_trim.json'
-valid_file='valid_no_dup_trim.json'
+
+train_file='train_no_dup.json'
+valid_file='valid_no_dup.json'
 
 #Actual Values from the training dataset:
 mean_data=[0.7170, 0.6794, 0.6613]
@@ -19,21 +22,23 @@ std_data=[0.2358, 0.2511, 0.2574]
 normalize = transforms.Normalize(mean=mean_data, std=std_data)
 transform_list=transforms.Compose([transforms.Scale(224), transforms.CenterCrop(224), transforms.ToTensor(), normalize,])
 
-num_epochs=10
-learning_rate=1e-4
+# num_epochs=1
+learning_rate=4e-5
 
-#Output is a variable containing tuples of (image,pos_image) pairs
-data=Data_processing.datapairs_updated(datadir=rootdir,filename=train_file,transforms=transform_list)
-train_loader=DataLoader(data,batch_size=10,shuffle=False)
+#Output is a variable containing tuples of (image,pos_image) pairs:
+data=Data_processing.style_labels(datadir=rootdir,filename=train_file)
+train_loader=DataLoader(data,batch_size=10,shuffle=True)
 
-#Initializing a vgg net model with an embedding of 20 size, using a pre-trained model but will update all the weights
+val_data=Data_processing.style_labels(datadir=rootdir,filename=valid_file)
+val_loader=DataLoader(val_data,batch_size=5,shuffle=False)
+
+#Import the model:
 vgg = Model.initialize_model("vgg", 128, feature_extract=False, use_pretrained=True)
 vgg.to(device)
 
-#Initializing the optimizer
+#Defining the optimizer and lr scheduler:
 optimizer=optim.Adam(vgg.parameters(),lr=learning_rate)
+scheduler = StepLR(optimizer, step_size=1, gamma=0.75)
 
-#Training:
-Model.train_model_oneneg(rootdir=rootdir, model=vgg ,dataloader=train_loader, optimizer=optimizer,epochs=num_epochs, device=device, validation_file=valid_file)
-print("Done")
-
+#Training the model:
+Model.tnet(model=vgg, dataloader=train_loader,valid_loader= val_loader, optimizer=optimizer, scheduler=scheduler, epochs=5, transforms=transform_list,device=device, validating=True)
