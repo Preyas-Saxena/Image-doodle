c2a7268cc63f1c0b74603da1dc82e7eb54d8712b
Preyas-Saxena <preyas.saxena@gmail.com>
2020-05-29 14:16:50 +0200

    Update Data_processing.py

--- Data_processing.py
+++ Data_processing.py
@@ -1,136 +1,25 @@
-
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 import json
-from PIL import Image
-import random
 
-imgdir='/data/leuven/329/vsc32927/images/'
+class style_labels(Dataset):
 
-#Defining a default image loader given the image's path:
-def default_image_loader(path):
-    return Image.open(path).convert('RGB')
+    def __init__(self, datadir, filename):
+        self.outfit_data = json.load(open(str(datadir + filename), 'r'))
 
-class datapairs_updated(Dataset):
-
-    def __init__(self,datadir,filename,transforms=None):
-
-        self.outfit_data=json.load(open(str(datadir+filename),'r'))
-        pos_pairs = []
-        max_items = 0
-
+        tot_outfits=0
+        outfit_labels=[]
         for outfit in self.outfit_data:
-            items = outfit['items']
-            cnt = len(items)
-            max_items = max(cnt, max_items)
-            outfit_id = outfit['set_id']
-            for j in range(cnt - 1):
-                for k in range(j + 1, cnt):
-                    pos_pairs.append([outfit_id, items[j]['index'], items[k]['index']])
+            tot_outfits=tot_outfits+1
+            outfit_id=outfit['set_id']
+            items= outfit['items']
+            outfit_labels.append([outfit_id, len(items)])
 
-        im2type = {}
-        category2ims = {}
-
-        for outfit in self.outfit_data:
-            outfit_id = outfit['set_id']
-            for item in outfit['items']:
-                im = item['index']
-                category = item['categoryid']
-
-                if outfit_id not in im2type:
-                    im2type[outfit_id] = {}
-
-                if im not in im2type[str(outfit_id)]:
-                    im2type[str(outfit_id)][im]=category
-
-                if category not in category2ims:
-                    category2ims[category] = {}
-
-                if outfit_id not in category2ims[category]:
-                    category2ims[category][outfit_id] = []
-
-                category2ims[category][outfit_id].append(im)
-
-        self.pos_pairs=pos_pairs
-        self.category2ims=category2ims
-        self.im2type=im2type
-        self.transform=transforms
-
-#Writing function for retrieving negative samples same as the item's type but from different style sets
-    def negative_samples(self,outfit_id,anchor_index):
-        anchor_catg=self.im2type[str(outfit_id)][anchor_index]
-        candidate_sets=self.category2ims[anchor_catg]
-        neg_samples=[]
-        ctr = 2
-
-        for key in candidate_sets.keys():
-            if len(neg_samples)<ctr:
-                if str(outfit_id)==key:
-                    pass
-                else:
-                    neg_samples.append((key,candidate_sets[key][0]))
-            else:
-                break
-        return neg_samples
-
-# Writing function for retrieving random negative samples:
-    def random_negative_samples(self, outfit_id):
-        outfits = list(self.im2type)
-        outfits.remove(outfit_id)
-        neg_samples = []
-        ctr = 3
-
-        for i in range(ctr):
-            random_outfit = random.choice(outfits)
-            outfits.remove(random_outfit)
-            candidate_items = self.im2type[random_outfit]
-            random_item = random.choice(list(candidate_items))
-            neg_samples.append((random_outfit, random_item))
-
-        return neg_samples
-
-    def load_train_items(self,path):
-        img=default_image_loader(path)
-        if self.transform is not None:
-            return self.transform(img)
-        else:
-            return img
+        self.tot_outfits= tot_outfits
+        self.outfit_labels= outfit_labels
 
     def __len__(self):
-        return len(self.pos_pairs)
+        return self.tot_outfits
 
-    def __getitem__(self, index):
-
-        outfit_id, anchor_im, pos_im = self.pos_pairs[index]
-        neg_list=self.negative_samples(outfit_id,anchor_im)
-        random_neg_list= self.random_negative_samples(outfit_id)
-        len_negs= len(neg_list)
-
-        if len_negs<2:
-            pending= 2-len_negs
-            difference= [item for item in random_neg_list if item not in neg_list]
-            for i in range(pending):
-                random_sample = random.choice(difference)
-                neg_list.append(random_sample)
-                difference.remove(random_sample)
-
-            len_negs=len(neg_list)
-
-        assert len_negs>=2
-
-        neg_outfit1,neg_im1=neg_list[0]
-        neg_outfit2,neg_im2=neg_list[1]
-
-        img1_path=imgdir+str(outfit_id)+'/'+str(anchor_im)+".jpg"
-        img2_path=imgdir+str(outfit_id)+'/'+str(pos_im)+".jpg"
-
-        img3_path=imgdir+neg_outfit1+'/'+str(neg_im1)+".jpg"
-        img4_path=imgdir+neg_outfit2+'/'+str(neg_im2)+".jpg"
-
-        img1 = self.load_train_items(img1_path)
-        img2 = self.load_train_items(img2_path)
-
-        img3 = self.load_train_items(img3_path)
-        img4 = self.load_train_items(img4_path)
-
-        return img1,img2, img3
-
+    def __getitem__(self, idx):
+        outfit_id, num_items= self.outfit_labels[idx]
+        return int(outfit_id), int(num_items)
